225311ab0ce0212a23fa7439594fef391ba5fca9
Guinslym <guinslym@gmail.com>
2017-05-31 16:32:07 -0400

    bootstrap;

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -10,9 +10,9 @@
         </div>
         <div class="navbar-collapse collapse" id="navbar-main">
           <ul class="nav navbar-nav navbar-right">
-            <li><a href="/">Stats</a></li>
-            <li><a href="/">About</a></li>
-            <li><a href="/">Download</a></li>
+            <li><a href="/stats">Stats</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/download">Download</a></li>
           </ul>
 
         </div>
