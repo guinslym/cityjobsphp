8c9f97a06fc2d56c1b1f9245c3779f58b3bd41eb
Guinslym <guinslym@gmail.com>
2017-05-31 13:00:31 -0400

    new day

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,15 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+// Patterns
+Route::pattern('id', '\d+');
+Route::pattern('hash', '[a-z0-9]+');
+Route::pattern('hex', '[a-f0-9]+');
+Route::pattern('uuid', '[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}');
+Route::pattern('base', '[a-zA-Z0-9]+');
+Route::pattern('slug', '[a-z0-9-]+');
+Route::pattern('username', '[a-z0-9_-]{3,16}');
+// make more of your own to suit your needs: email, password, etc.
 
 
 //homeListView
@@ -30,7 +38,7 @@ Route::get('download', 'EmploiController@download')->name('download');
 
 //Statistics (JSON)
 Route::get('/statistics', 'EmploiController@showStatistics')->name('stats');
-Route::get('/statistics/{annee}/{mois}/{jour}', 'EmploiController@showStatisticsJSON')->name('statsJSON');
+Route::get('/statistics/{annee}/{mois}/{jour}', 'EmploiController@showStatisticsJSON')->name('statsJSON')->where('annee', '^(19|20)\d{2}$')->where('mois', '^(19|20)\d{2}$')->where('jour', '^(19|20)\d{2}$');
 
 //Search View (get)
 Route::get('search/{searchkey}', 'EmploiController@search')->where('searchkey', '[A-Za-z]+')->name('search');
